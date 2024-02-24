bca8618ece11007bfd05e3b95d0bfac75ae80837
odho <mubashirodho@yahoo.com>
2024-02-24 05:24:47 +0500

    count when scroll

--- src/components/MainHome/CounterSec.js
+++ src/components/MainHome/CounterSec.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CountUp } from "use-count-up";
 
 export const CounterSec = () => {
+  const [startCounting, setStartCounting] = useState(false);
+
+  // Use useEffect to start counting when the component mounts
+  useEffect(() => {
+    const handleScroll = () => {
+      const counterSection = document.querySelector(".counter");
+      const counterSectionTop = counterSection.getBoundingClientRect().top;
+
+      // If the counter section is in the viewport, start counting
+      if (counterSectionTop < window.innerHeight) {
+        setStartCounting(true);
+        // Remove the scroll event listener to avoid unnecessary calculations
+        window.removeEventListener("scroll", handleScroll);
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Empty dependency array ensures this effect runs only once
+
   return (
     <>
       <section className="counter">
@@ -11,7 +36,7 @@ export const CounterSec = () => {
               <div className="count-box">
                 <i className="icofont-simple-smile"></i>
                 <span data-toggle="counter-up">
-                <CountUp  isCounting end={372} duration={3.2} />
+                <CountUp  isCounting={startCounting} end={372} duration={3.2} />
                 </span>
                 <hr className="hrborder" />
                 <p>Satisfied Clients</p>
@@ -20,7 +45,7 @@ export const CounterSec = () => {
             <div className="col-lg-3 col-md-6 col-6 mt-5 mt-lg-0">
               <div className="count-box">
                 <i className="icofont-document-folder"></i>
-                <span data-toggle="counter-up"><CountUp  isCounting end={13} duration={3.2} /></span>
+                <span data-toggle="counter-up"><CountUp  isCounting={startCounting} end={13} duration={3.2} /></span>
                 <hr className="hrborder" />
                 <p>Manufacturing Partners</p>
               </div>
@@ -28,7 +53,7 @@ export const CounterSec = () => {
             <div className="col-lg-3 col-md-6 col-6 mt-5 mt-lg-0">
               <div className="count-box">
                 <i className="icofont-live-support"></i>
-                <span data-toggle="counter-up"><CountUp  isCounting end={57} duration={3.2} /></span>
+                <span data-toggle="counter-up"><CountUp  isCounting={startCounting} end={57} duration={3.2} /></span>
                 <hr className="hrborder" />
                 <p>Participated Projects</p>
               </div>
@@ -36,7 +61,7 @@ export const CounterSec = () => {
             <div className="col-lg-3 col-md-6 col-6 mt-5 mt-lg-0">
               <div className="count-box">
                 <i className="icofont-users-alt-5"></i>
-                <span data-toggle="counter-up"><CountUp  isCounting end={5} duration={3.2} /></span>
+                <span data-toggle="counter-up"><CountUp  isCounting={startCounting} end={5} duration={3.2} /></span>
                 <hr className="hrborder" />
                 <p>Countries worldwide</p>
               </div>
