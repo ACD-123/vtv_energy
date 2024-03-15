7290e2a6fcfd1d6dd02d240160bcaa507a052840
odho <mubashirodho@yahoo.com>
2024-03-16 03:31:03 +0500

    new

--- src/components/MainHome/Hero.js
+++ src/components/MainHome/Hero.js
@@ -6,22 +6,13 @@ export const Hero = () => {
     <section className="hero">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-12 hero-leftMain">
+          <div className="col-lg-12 col-sm-12 hero-leftMain">
             <div className='herofirst'>
-              <h1 className='text-white'>Commercial Solar</h1>
+              <h1 className='text-white'>Turn-key engineering solutions</h1>
               <p className='px-6 paraBG'>
-              Explore more
+              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
               </p>
-              {/* <button className="btn btn-primary">Get A Quote</button> */}
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-6 col-sm-12 hero-rightMain">
-          <div className='herofirst'>
-              <h1 className='text-white'>Residential Solar</h1>
-              <p className='paraBG px-6'>
-              Explore more
-              </p>
-              {/* <button className="btn btn-primary">Get A Quote</button> */}
+              <button className="buttonMainHomeBanner">Let’s Go!  Zero carbon footprint</button>
             </div>
           </div>
         </div>
--- src/components/Navbar/Topbar.js
+++ src/components/Navbar/Topbar.js
@@ -1,36 +1,34 @@
-import React from 'react'
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; // Import NavLink instead of Link
+
 export const Topbar = () => {
   return (
-   <>
- <Navbar collapseOnSelect expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-        <Link to="/"><img width={220} src={require("../assets/logo/headerLogo.png")} alt="Google" /></Link>
-
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" style={{
-    justifyContent: "space-around"
-  }}>
-          <Nav className='menunav'>
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/solar-energy" className="nav-link">Solar Energy</Link>
-            <Link to="/wind-energy" className="nav-link">Wind energy</Link>
-            <Link to="Why-VTV" className="nav-link">Why VTV</Link>
-            <Link to="/contact-us" className="nav-link"> Contact & Support</Link>
-          </Nav>
-          <Nav>
-            <Button className='px-4 menuButton'>Get A Quote</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-   </>
+    <>
+      <Navbar collapseOnSelect expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <NavLink to="/"><img width={220} src={require("../assets/logo/headerLogo.png")} alt="Google" /></NavLink>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" style={{ justifyContent: "space-around" }}>
+            <Nav className='menunav'>
+              <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+              <NavLink to="/solar-energy" className="nav-link" activeClassName="active">Solar Energy</NavLink>
+              <NavLink to="/wind-energy" className="nav-link" activeClassName="active">Wind energy</NavLink>
+              <NavLink to="/Why-VTV" className="nav-link" activeClassName="active">Why VTV</NavLink>
+              <NavLink to="/contact-us" className="nav-link" activeClassName="active">Contact & Support</NavLink>
+            </Nav>
+            <Nav>
+              <Button className='px-4 menuButton'>Get A Quote</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   )
 }
